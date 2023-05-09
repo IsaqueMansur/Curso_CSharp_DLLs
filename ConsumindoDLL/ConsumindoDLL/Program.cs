c675bd7cad87a551000d2cde911def9ea7045ceb
IsaqueMansur <isaquemansur@gmail.com>
2023-05-09 10:13:52 -0300

    building and using a dll with C#

---
+++ ConsumindoDLL/ConsumindoDLL/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CriandoDLL;
+
+namespace ConsumindoDLL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CriandoDLL.Mathematic.Sum(2, 2));
+            Console.WriteLine(CriandoDLL.Mathematic.Multiply(2, 2));
+        }
+    }
+}
---
+++ CriandoDLL/CriandoDLL/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriandoDLL
+{
+    public class Mathematic
+    {
+        public static double Sum(double a, double b)
+        {
+            return a + b;
+        }
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
